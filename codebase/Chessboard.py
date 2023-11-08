6c6b56b9418c48a9d5fddee4ab1cd43321c0bf87
bisterj <69654276+bisterj@users.noreply.github.com>
2023-11-08 10:30:38 -0500

    Added logo and title

--- codebase/Chessboard.py
+++ codebase/Chessboard.py
@@ -17,12 +17,16 @@ board = [[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
 
 root = Tk()
 
+root.title("CHESSTRAINER")
+
+logo = PhotoImage(file="chess_logo.png")
+root.iconphoto(False, logo)
+
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="CHESSTRAINER").grid(row=0, column=0, pady=0)
-ttk.Button(frm, text="Start").grid(column=1, row=1, ipadx=20, ipady=10, pady=20)
-ttk.Button(frm, text="About").grid(column=1, row=2, ipadx=20, ipady=10, pady=20)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=3, ipadx=20, ipady=10, pady=20)
+ttk.Button(frm, text="Start").grid(column=1, row=0, ipadx=20, ipady=10, pady=20)
+ttk.Button(frm, text="About").grid(column=1, row=1, ipadx=20, ipady=10, pady=20)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=2, ipadx=20, ipady=10, pady=20)
 
 canvas = Canvas(root, width=480, height=480)
 canvas.create_rectangle(0, 0, 480, 480, fill="#769656", outline="black")
