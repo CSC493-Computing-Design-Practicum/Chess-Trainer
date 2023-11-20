4cb3f4252fa1ad1bcd95798d902d72886118088c
bisterj <69654276+bisterj@users.noreply.github.com>
2023-11-20 09:33:00 -0500

    Fixed evaluation for weak_moves so that white and black are handled seperately.

--- codebase/main.py
+++ codebase/main.py
@@ -65,20 +65,24 @@ def analyse(games):
             stockfish.set_fen_position(board.fen())
             newval = stockfish.get_evaluation()["value"]
             if r % 2 == 0:
+                # White move
                 score = newval - previousval
-            else:
-                score = -(newval - previousval)
-            if score < -100:
-                if r % 2 == 0:
+                if score < -100:
                     weak_moves.append((i, r, score))
-                else:
-                    weak_moves.append((i, r, -score))
+            else:
+                score = newval - previousval
+                if score > 100:
+                    weak_moves.append((i, r, score))
 
     return weak_moves
 
 
-cleanedgames = import_games(10, 2023)
-weak_moves = analyse(cleanedgames)
+# cleanedgames = import_games(11, 2023)
+# weak_moves = analyse(cleanedgames)
+
+cleanedgames = [['e4', 'e5', 'Nf3', 'd6', 'Bc4', 'Be6', 'Bxe6', 'fxe6', 'd4', 'exd4', 'Nxd4', 'Qf6', 'Nc3', 'Nc6', 'Nxc6', 'bxc6', 'O-O', 'd5', 'Qe1', 'Bd6', 'exd5', 'cxd5', 'Nxd5', 'Kd7', 'Nxf6+', 'Nxf6', 'Bg5'], ['e4', 'd5', 'exd5', 'Qxd5', 'Nc3', 'Qa5', 'Bc4', 'e5', 'Qf3', 'Be6', 'Bxe6', 'fxe6', 'Qxb7', 'Qb6', 'Qxa8', 'Be7', 'Qe4', 'Nf6', 'Qxe5', 'Nc6', 'Qg3', 'Nd4', 'Kd1', 'g6', 'Nf3', 'Nh5', 'Qe5', 'Nxf3', 'Qxh8+', 'Kf7', 'Qxh7+', 'Kf6', 'Qh8+', 'Kg5', 'gxf3', 'Qxf2', 'Qe5+', 'Kh6', 'd4+', 'Kh7', 'Bg5', 'Bb4', 'Ne4', 'Qg2', 'Re1', 'Bxe1', 'Kxe1', 'Qxc2', 'Qxe6', 'Qxb2', 'Qf7+', 'Ng7', 'Rc1', 'Qb4+', 'Kf2', 'Qb2+', 'Kg3', 'Qxd4', 'Nf6+', 'Kh8', 'Qg8#']]
+weak_moves = [(0, 23, 545), (1, 0, -665), (1, 8, -112), (1, 9, 478), (1, 20, -173), (1, 22, -118), (1, 23, 114), (1, 27, 207), (1, 29, 153), (1, 31, 310), (1, 32, -343), (1, 33, 309), (1, 35, 287), (1, 36, -3337), (1, 37, 425), (1, 38, -3211), (1, 40, -205), (1, 41, 241), (1, 42, -1082), (1, 43, 977), (1, 44, -179), (1, 45, 3489), (1, 46, -3211)]
+
 
 print(weak_moves)
 
@@ -89,7 +93,7 @@ while(answer == "y"):
     if quiz[1] % 2 == 0:
         print("White played " + cleanedgames[quiz[0]][quiz[1]] + " find a better move.")
     else:
-        print("Black played" + cleanedgames[quiz[0]][quiz[1]] + " find a better move.")
+        print("Black played " + cleanedgames[quiz[0]][quiz[1]] + " find a better move.")
 
     board = chess.Board()
     for i in range(0, quiz[1]):
