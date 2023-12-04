b1fc33d481f32c82002979c298bba08729a45f0b
bisterj <69654276+bisterj@users.noreply.github.com>
2023-12-04 09:41:19 -0500

    Added some documentation

--- codebase/ChessPiece.py
+++ codebase/ChessPiece.py
@@ -8,20 +8,38 @@ class ChessPiece:
         return f"This is a {self.color} {self.piece} at {self.pos}."
 
     def get_color(self):
+        """
+        Simple get function for color.
+        :return: the color
+        """
         return self.color
 
     def get_piece(self):
+        """
+        Simple get function for piece.
+        :return: the type of piece it is ex. bishop
+        """
         return self.piece
 
     def get_pos(self):
+        """
+        Simple get function for position
+        :return: the position of the piece
+        """
         return self.pos
 
     def get_chess_pos(self):
+        """
+        Tells you what the position of the piece is in terms of a chess board.
+        :return: the chess board position of the piece
+        """
         lookup = ["a", "b", "c", "d", "e", "f", "g", "h"]
         return f"{lookup[self.pos[1]]}{8 - self.pos[0]}"
 
     def move(self, pos):
+        """
+        Moves the piece to the position
+        :param pos: the postion to move the piece to.
+        :return: None
+        """
         self.pos = pos
-
-    def raw_moves(self):
-        pass
